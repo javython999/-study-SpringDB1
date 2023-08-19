1cd9656a0ad69ab628b4b3ef2aaf72846c6326a7
javython999 <infinity2544@gmail.com>
2023-08-19 16:04:53 +0900

    init

---
+++ src/main/java/hello/jdbc/HelloJdbcApplication.java
@@ -0,0 +1,13 @@
+package hello.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloJdbcApplication.class, args);
+    }
+
+}
